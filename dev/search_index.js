49c051baea5c868b2be71870430270c83e6151af
Documenter.jl <documenter@juliadocs.github.io>
2023-08-10 01:12:59 +0000

    build based on bf6d1de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WorkingScopes","category":"page"},{"location":"#WorkingScopes","page":"Home","title":"WorkingScopes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WorkingScopes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WorkingScopes]","category":"page"}]
+}
